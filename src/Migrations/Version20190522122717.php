e3c4c698948467e1ca9bfddb5bb5f579a7515d35
gobshido <gobshido@gmail.com>
2019-05-22 14:29:12 +0200

    Prix OK

--- src/Migrations/Version20190522122717.php
+++ src/Migrations/Version20190522122717.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190522112535 extends AbstractMigration
+final class Version20190522122717 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,7 @@ final class Version20190522112535 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE actualite ADD image_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE actualite ADD CONSTRAINT FK_549281973DA5256D FOREIGN KEY (image_id) REFERENCES image (id)');
-        $this->addSql('CREATE INDEX IDX_549281973DA5256D ON actualite (image_id)');
+        $this->addSql('CREATE TABLE prix (id INT AUTO_INCREMENT NOT NULL, prix_particulier VARCHAR(255) DEFAULT NULL, prix_entreprise VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -32,8 +30,6 @@ final class Version20190522112535 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE actualite DROP FOREIGN KEY FK_549281973DA5256D');
-        $this->addSql('DROP INDEX IDX_549281973DA5256D ON actualite');
-        $this->addSql('ALTER TABLE actualite DROP image_id');
+        $this->addSql('DROP TABLE prix');
     }
 }
